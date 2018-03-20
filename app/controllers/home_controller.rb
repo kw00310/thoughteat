ea5249674a3aab04f7d91ace0e272c2e94b7669e
Kane Walsh <kane_walsh@hotmail.com>
2018-03-20 20:45:23 +0000

    structureUpdate

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,12 @@
+class HomeController < ApplicationController
+    def home
+    end
+    def foodrecipes
+    end
+    def fitness
+    end 
+    def community
+    end 
+    def contact
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root 'home#home', as: "home"
+  get "thoughtfuleating/app/views/home/foodrecipes.html.haml", to: "home#foodrecipes", as: "foodrecipes"
+  get "thoughtfuleating/app/views/home/fitness.html.haml", to: "home#fitness", as: "fitness"
+  get "thoughtfuleating/app/views/home/community.html.haml", to: "home#community", as: "community"
+  get "thoughtfuleating/app/views/home/contact.html.haml", to: "home#contact", as: "contact"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
